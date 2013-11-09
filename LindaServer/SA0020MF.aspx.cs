8c2a76bfb8bd01c3eaec8c01f187e8dd1fd1a4b9
zero781028 <zero781028@gmail.com>
2013-11-09 15:45:13 +0800

    //

---
+++ LindaServer/SA0020MF.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LindaServer
+{
+    public partial class SA0020MF : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
